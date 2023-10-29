05b6171a2904c18d847bfe78dc4e05982394e624
Shreyas Ravi <shreyas.ravi@tufts.edu>
2023-10-29 15:49:46 -0400

    switch mongo.js to mongo.ts

--- src/lib/mongodb.js
+++
@@ -1,6 +0,0 @@
-import { MongoClient } from 'mongodb'
-
-const uri = "mongodb+srv://biwomenquarterly:JumboCode2324@biwomenquarterly.17vcngj.mongodb.net/";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-export default client;
