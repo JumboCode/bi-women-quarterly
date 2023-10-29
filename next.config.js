a526b8300f5648c3b143c8bd3a970b8dcfad48fd
Shreyas Ravi <shreyas.ravi@tufts.edu>
2023-10-29 17:04:59 -0400

    google api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const nextConfig = {
+    webpack: (config) => {
+        config.resolve.fallback = {
+            fs: false,
+            net: false,
+            tls: false,
+            http2: false,
+            dns: false,
+            child_process: false
+        };
+
+        return config;
+    },
     reactStrictMode: true
 };
 
--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,6 +1,7 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient } from "mongodb";
 
-const uri: string = "mongodb+srv://biwomenquarterly:JumboCode2324@biwomenquarterly.17vcngj.mongodb.net/";
+const uri: string =
+    "mongodb+srv://biwomenquarterly:JumboCode2324@biwomenquarterly.17vcngj.mongodb.net/";
 const client: MongoClient = new MongoClient(uri);
 
 export default client;
