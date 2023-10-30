4f7a9f5c4c428e30e9175d01161dfef2c29e9994
Allison Zhang <andih@Andis-MacBook-Pro.local>
2023-10-30 11:26:22 -0400

    made code cleaner

--- src/pages/components/UserForm.tsx
+++ src/pages/components/UserForm.tsx
@@ -1,63 +1,55 @@
+// Form to collect pen name, email, short biography, social media
+// On submission, prints user info to console
+
 import {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 
 export function MyForm() {
-  // const [name, setName] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [bio, setBio] = useState("");
-  // const [socMedia, setSocMedia] = useState("");
-  
-  
-  
-  const [user, setUser] = useState({name : "",
-                                    email : "",
-                                    bio : "",
-                                    socMedia : ""})
-
-                                  
-                                
-  const onChangeUser = (e : React.ChangeEvent<HTMLInputElement>) => {
-    setUser({...user, [e.target.name] : e.target.value});
-  }
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    alert(`The name you entered was: ${user.name}`);
-    alert(`The email you entered was: ${user.email}`);
-    alert(`The bio you entered was: ${user.bio}`);
-    alert(`The social media accounts you entered were: ${user.socMedia}`);
-    console.log(user);
-  }                       
-  return (
-      <form onSubmit={handleSubmit}>
-        <label>Enter your pen name:
-          <input type="text" 
-                 value={user.name}
-                 name="name"      
-                 onChange={onChangeUser}/>
-        </label>
-        <br></br>
-        <label>Enter your email address:
-          <input type="text"
-                 value={user.email}
-                 name="email"
-                 onChange={onChangeUser}/>
-        </label>
-        <br></br>
-        <label>Enter a short biography:
-          <input type="text" 
-                 value={user.bio}
-                 name="bio"
-                 onChange={onChangeUser}/>
-        </label>
-        <br></br>
-        <label>Enter any social media accounts:
-          <input type="text" 
-                 value={user.socMedia}
-                 name="socMedia"
-                 onChange={onChangeUser}/>
-        </label>
-        <br></br>
-        <button type = "submit">Submit</button>
-      </form>
-    )
+    const [user, setUser] = useState({name : "",
+                                      email : "",
+                                      bio : "",
+                                      socMedia : ""})    
+    
+    const onChangeUser = (e : React.ChangeEvent<HTMLInputElement>) => {
+        setUser({...user, [e.target.name] : e.target.value});
+    }
+    
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(user);
+    }              
+            
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Enter your pen name:
+                <input type="text" 
+                      value={user.name}
+                      name="name"      
+                      onChange={onChangeUser}/>
+            </label>
+            <br></br>
+            <label>Enter your email address:
+                <input type="text"
+                      value={user.email}
+                      name="email"
+                      onChange={onChangeUser}/>
+            </label>
+            <br></br>
+            <label>Enter a short biography:
+                <input type="text" 
+                      value={user.bio}
+                      name="bio"
+                      onChange={onChangeUser}/>
+            </label>
+            <br></br>
+            <label>Enter any social media accounts:
+                <input type="text" 
+                      value={user.socMedia}
+                      name="socMedia"
+                      onChange={onChangeUser}/>
+            </label>
+            <br></br>
+            <button type = "submit">Submit</button>
+        </form>
+      )
 }
