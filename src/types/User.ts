9e9ce22eb76b9355326082fd713aafca786fabcd
Nejmi Walid <walid@MacBook-Air-de-Nejmi.local>
2023-11-05 20:43:16 -0500

    Resolved comments

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,4 +1,14 @@
-// User type stores basic information about the user
+/**
+ * Represents basic information about a user.
+ * @author Walid and Geneva
+ * @typedef {Object} User
+ * @property {string} username - The username of the user.
+ * @property {string} penname - The pen name of the user.
+ * @property {string} email - The email address of the user.
+ * @property {string} bio - The biography of the user.
+ * @property {socialMedias} socials - The user's social media information.
+ * @property {string} headshot - The link to the user's headshot image.
+ */
 
 import socialMedias from "./socials";
 
