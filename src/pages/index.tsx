4a404cf7f63f30465fabfdbb8ad23506a4e3bc5e
Allison Zhang <andih@Andis-MacBook-Pro.local>
2023-11-05 20:39:50 -0500

    Added framework from the clerk tutorial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-    return <div>Hello</div>;
+    return (
+        <div>
+          <UserButton afterSignOutUrl="/"/>
+        </div>
+    )
 }
