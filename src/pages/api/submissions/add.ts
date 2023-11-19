50e7402fdc8b289f815f1c99a8ae2cf06b6ef0f7
austen_em <austen.money@gmail.com>
2023-11-19 00:20:16 -0500

    reorganized and removed testing

---
+++ src/pages/api/submissions/add.ts
@@ -0,0 +1,38 @@
+/**
+ * Submission upload endpoint for mongoDB Submissions collection
+ * @author Alana Sendlakowski
+ * @author Vanessa Rose
+ */
+
+// Import NextApiRequest and NextApiResponse
+import { NextApiRequest, NextApiResponse } from "next";
+
+// Import clientPromise
+import clientPromise from "@/lib/mongodb";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+    try {
+        const client = await clientPromise;
+        // acesses database BiWomenQuarterly
+        const db = client.db("BiWomenQuarterly");
+        // acesses collection Submissions
+        const collect = db.collection("Submissions");
+        
+        // Insert the defined document into the "Submissions" collection
+        const body = JSON.parse(req.body)
+        await collect.insertOne(body);
+
+        // accesses collection to verify that everything was inserted correct
+        const collection = await db
+            .collection("Submissions")
+            .find({})
+            .limit(30)
+            .toArray();
+
+            res.status(201).json({ success: true, data: collection });
+        
+    } catch (e) {
+        res.status(400).json({ success: false });
+    }
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,3 @@
 export default function Home() {
-    const fetchData = async () => {
-        await fetch('http://localhost:3000/api/mongo')
-            .then(res => res.json())
-            .then(res => {
-                if (res.success) {
-                    console.log("Successfully connected to database!");
-                    console.log(res.data);
-                } else {
-                    console.log("Could not connect to database!");
-                }
-            });
-    };
-    fetchData();
-
     return <div>Hello</div>;
 }
