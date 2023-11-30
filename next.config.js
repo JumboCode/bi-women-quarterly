8a7bf2ef066c497ad8bbeabb1a3d86568162842f
austen_em <austen.money@gmail.com>
2023-11-30 15:41:19 -0500

    moving stuff

--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
 /** @type {import('next').NextConfig} */
 /* eslint-disable @typescript-eslint/no-var-requires */
-module.exports = {
-    async rewrites() {
-        return [
-          {
-            source: '/api/:path*',
-            destination: 'https://api.example.com/:path*',
-          },
-        ]
-      },
-  };
+const nextConfig = {
+    webpack: config => {
+        config.resolve.fallback = {
+            fs: false,
+            net: false,
+            tls: false,
+            http2: false,
+            dns: false,
+            child_process: false
+        };
+
+        return config;
+    },
+    reactStrictMode: true
+};
+
+module.exports = nextConfig;
