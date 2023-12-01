2a8ffb972856e659923242123b93753968b6dfde
Avery Hanna <avery.hanna@tufts.edu>
2023-12-01 20:40:09 +0000

    cleanup

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -4,7 +4,6 @@
 import {useState} from 'react';
 import User from "../types/User";
 import SocialMedias from "../types/SocialMedias"
-import UploadFile from '@/pages/components/UploadFile';
 
 
 /**
