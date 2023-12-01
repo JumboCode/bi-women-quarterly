9da304d28e5b0ea6d301e8aa605eef69347a164e
shkim1211 <so_hyun.kim@tufts.edu>
2023-12-01 09:33:37 -0500

    adding files

---
+++ src/pages/api/mongoUser.ts
@@ -0,0 +1,36 @@
+//Connecting users to BiWomenQuarterly
+import { NextApiRequest, NextApiResponse } from "next";
+import clientPromise from "@/lib/mongodb";
+import User from "@/types/User";
+import SocialMedias from "@/types/SocialMedias";
+
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        // Receive the data from fetch() 
+        const user_example = JSON.parse(req.body);  
+        const user: User = user_example; 
+
+        const client = await clientPromise;
+        // replace database name with whatever you are testing
+        const db = client.db("BiWomenQuarterly");
+        const user_collection = db.collection("Users"); 
+        const result = await user_collection.insertOne(user); 
+
+        console.log("Printing collection:"); 
+        console.log(result);
+        res.status(201).json({ success: true, data: result.insertedId });
+
+        const sub_collection = await db
+         .collection("Users")
+         .find({})
+         .limit(10)
+         .toArray(); 
+        console.log(sub_collection); 
+
+    } catch (e) {
+        console.log(e);
+        res.status(400).json({ success: false });
+    }
+};
+
