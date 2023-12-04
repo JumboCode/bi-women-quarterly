0edd76f748238d5883eb9dfcfd2c838182b27787
vanessa3rose <vanessa3rose@gmail.com>
2023-12-03 20:58:30 -0500

    Starting to style the modal

--- src/components/SubmissionModal.tsx
+++ src/components/SubmissionModal.tsx
@@ -12,51 +12,54 @@ export default function SubmissionModal() {
       <>
         {showModal ? ( 
           <>
-            <div
-              className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-            >
-              <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+              <div className="w-screen  max-w-3xl border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                   
                   <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                    <div>
-                      <div>Submission Type *</div>
-                      <div>Submission Piece</div>
-                      <div>Optional Related Photo</div>
+                      <div>
+                        <div>Submission Type *</div>
+                        
+                        <fieldset>
+
+                        <div className="flex flex-col gap-2">
+                          <div className="inline-flex items-center">
+                            <label className="relative flex items-center p-3 rounded-full cursor-pointer">
+                              <input name="terms" type="radio" className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 checked:border-[#395fbb]"/>
+                              <span className="absolute transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 16 16" fill="#395fbb">
+                                  <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
+                                </svg>
+                              </span>
+                            </label>
+                            <label className="mt-px font-light text-gray-700 cursor-pointer select-none" htmlFor="html2"> Submission Piece </label>
+                          </div>
+
+                          <div className="inline-flex items-center">
+                            <label className="relative flex items-center p-3 rounded-full cursor-pointer">
+                              <input name="terms" type="radio" className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 checked:border-[#395fbb]"/>
+                              <span className="absolute transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 16 16" fill="#395fbb">
+                                  <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
+                                </svg>
+                              </span>
+                            </label>
+                            <label className="mt-px font-light text-gray-700 cursor-pointer select-none" htmlFor="html2"> Optional Related Photo </label>
+                          </div>
+                        </div>
+                        </fieldset>
+
+                      </div>
+                      <button onClick={() => setShowModal(false)}> x </button>
                     </div>
-                    <button onClick={() => setShowModal(false)}> x </button>
-                  </div>
-                  <div>Title of Piece *</div>
-                  {/*body*/}
-                  <div className="relative p-6 flex-auto">
-                    <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
-                      I always felt like I could do anything. That’s the main
-                      thing people are controlled by! Thoughts- their perception
-                      of themselves! They're slowed down by their perception of
-                      themselves. If you're taught you can’t do anything, you
-                      won’t do anything. I was taught I could do everything.
-                    </p>
-                  </div>
-                  {/*footer*/}
+                    <div>Title of Piece *</div>
+                    <div>Description</div>
+
                   <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                    <button
-                      className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                      type="button"
-                      onClick={() => setShowModal(false)}
-                    >
-                      Close
-                    </button>
-                    <button
-                      className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                      type="button"
-                      onClick={() => setShowModal(false)}
-                    >
-                      Save Changes
-                    </button>
+                    <button className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="button" onClick={() => setShowModal(false)}>Done</button>
                   </div>
                 </div>
               </div>
-            </div>
             <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
           </>
         ) : null}
