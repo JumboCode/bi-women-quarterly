2df62a45d3f730c24b013a7db462512a26fe3c4f
Allison Zhang <andih@Andis-MacBook-Pro.local>
2023-12-05 10:57:06 -0500

    first commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ["/api/webhook", "/submit", "/"],
-    ignoredRoutes: ["/api/submissions/get"]
+  publicRoutes: ["/", "/api/webhook"],
 });
 
 export const config = {
