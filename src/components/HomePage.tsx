5ce73c9528752f2a075b983cddd1e209639d5f6e
Nejmi Walid <walid@MacBook-Air-de-Nejmi.local>
2023-12-07 16:29:56 -0500

    Updated routes for mdidleware

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -144,7 +144,7 @@ export default function HomePage() {
     /*------------------------------------------------------------------------*/
     
     if (!user) {
-        return;
+        return null;
     }
     /**
      * Add new submission
