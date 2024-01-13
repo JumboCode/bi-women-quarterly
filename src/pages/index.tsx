55cf624fa8bd44d7a75cf785cc49c43b2333a41e
austen_em <austen.money@gmail.com>
2024-01-13 00:38:59 -0500

    added home page back

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
 import HomePage from "@/components/HomePage";
-import UserForm from '@/components/UserForm';
 
 export default function Home() {
     return (
         <div>
-            <UserForm></UserForm>
+            <HomePage></HomePage>
         </div>
     );
 }
