0cfaecee8cbb2eb5f291cd25ec41dd2089c528b3
Walid Nejmi <wal.nejmi@gmail.com>
2024-01-17 22:10:18 -0500

    Testing submissions

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ["/api/webhook", "/submit"]
+    publicRoutes: ["/api/webhook", "/submit", "/api/submissions/edit", "/api/submissions/add", "/api/submissions/get"]
 });
 
 export const config = {
