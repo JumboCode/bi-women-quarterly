1a477622af232b1f031a9765eb8e63e4a00ea088
Avery Hanna <avery.hanna@tufts.edu>
2024-01-27 11:51:53 -0500

    added add submission to publicRoutes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ["/api/webhook", "/submit", "/"],
+    publicRoutes: ["/api/webhook", "/submit", "/", "/api/submissions/add"],
     ignoredRoutes: ["/api/submissions/get"]
 });
 
