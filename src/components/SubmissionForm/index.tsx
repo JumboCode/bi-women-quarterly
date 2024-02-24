7535cf81e779bafd2656935ce0790d359151a265
shkim1211 <so_hyun.kim@tufts.edu>
2024-02-24 01:39:17 -0500

    changed new submission view's format to follow the page design

--- src/components/SubmissionForm/index.tsx
+++ src/components/SubmissionForm/index.tsx
@@ -276,13 +276,11 @@ export default function SubmissionForm() {
                     </button>
                 </div>
             {/* // Creates a form to retrieve title, issue, and name information */}
-                <div>
+                <div className="grid grid-cols-2 gap-4">
                     {/* drop down element for issue selection */}
                     <h1 className="text-2xl font-bold pb-8 mt-3 ml-24 justify=">New Submission</h1>
-                    
                     <div className="flex md:flex md:flex-grow flex-row justify-end space-x-1 px-[20px] py-[10px]">
-                        <label className="pr-[6px] font-bold"> *Issue: </label>
-                        <select name="issue" className="inline-block h-[30px] w-[115px] pl-1 text-m text-gray-900 rounded-lg" value={submission.issue} onChange={handleSubmissionChange}>
+                        <select name="issue" className="absolute right-[208px] h-[30px] w-[115px] pl-1 text-m text-gray-900 rounded-lg" value={submission.issue} onChange={handleSubmissionChange}>
                             <option defaultValue="Select Issues">Select Issue</option> 
                                 <option value="Any">Any</option>
                                 {
@@ -294,9 +292,8 @@ export default function SubmissionForm() {
                                 }
                         </select>
     
-                    <label className="pl-[50px] pr-[6px] font-bold">*Type: </label>
                         {/* drop down element for type selection */}
-                        <select name="medium" className="h-[30px] w-[115px] pl-1 text-m text-gray-900 rounded-lg" value={submission.medium} onChange={handleSubmissionChange}>
+                        <select name="medium" className="absolute right-[80px] h-[30px] w-[115px] pl-1 text-m text-gray-900 rounded-lg" value={submission.medium} onChange={handleSubmissionChange}>
                             <option defaultValue="Select Type">Select Type</option>
                             <option value={Mediums.Fiction}>{Mediums.Fiction}</option>
                             <option value={Mediums.Nonfiction}>{Mediums.Nonfiction}</option>
@@ -306,31 +303,54 @@ export default function SubmissionForm() {
                         </select>
                     </div>
                 </div>
-                <div className="p-6 h-[250px] bg-[#c3cee3] rounded-xl shadow-lg items-center space-x-4 outline-dashed outline-[#768fcd] outline-offset-[-3px]">
-                <div className="break-normal">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#3b60ba" className="mx-auto flex h-20 w-20 items-center justify-center">
-                        <path fillRule="evenodd" d="M10.5 3.75a6 6 0 00-5.98 6.496A5.25 5.25 0 006.75 20.25H18a4.5 4.5 0 002.206-8.423 3.75 3.75 0 00-4.133-4.303A6.001 6.001 0 0010.5 3.75zm2.03 5.47a.75.75 0 00-1.06 0l-3 3a.75.75 0 101.06 1.06l1.72-1.72v4.94a.75.75 0 001.5 0v-4.94l1.72 1.72a.75.75 0 101.06-1.06l-3-3z" clipRule="evenodd" />
-                    </svg>
-                </div>
-    
-                    <h1 className="flex grow text-center justify-center text-l font-bold pb-1 pt-1">Drag & Drop Files Here</h1>
-                    <h1 className="flex grow text-center justify-center text-m pb-1 pt-1">or</h1>
-                    
-                    <div className="flex grow p-[10px] w-screen justify-center text-[#3b60ba]">
-                        <LocalFile handleNewPreview={handleNewPreview}/>
-                        <button type="submit" className="h-[30px] w-[115px] absolute right-[200px] rounded-sm items-center outline outline-[#5072c0] outline-offset-[3px]">
+                {/* Submission Boxes */}
+                <div className="grid grid-cols-2 place-items-center">
+                    {/* Submission Box 1 */}
+                    <div className="p-6 h-[250px] w-[550px] bg-[#c3cee3] rounded-xl shadow-lg items-center space-x-4 outline-dashed outline-[#768fcd] outline-offset-[-3px]">
+                        <div className="break-normal">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#3b60ba" className="mx-auto flex h-20 w-20 items-center justify-center">
+                                <path fillRule="evenodd" d="M10.5 3.75a6 6 0 00-5.98 6.496A5.25 5.25 0 006.75 20.25H18a4.5 4.5 0 002.206-8.423 3.75 3.75 0 00-4.133-4.303A6.001 6.001 0 0010.5 3.75zm2.03 5.47a.75.75 0 00-1.06 0l-3 3a.75.75 0 101.06 1.06l1.72-1.72v4.94a.75.75 0 001.5 0v-4.94l1.72 1.72a.75.75 0 101.06-1.06l-3-3z" clipRule="evenodd" />
+                            </svg>
+                        </div>
+            
+                        <h1 className="flex grow text-center justify-center text-l font-bold pb-1 pt-1">Drag & Drop Files Here</h1>
+                        <h1 className="flex grow text-center justify-center text-m pb-1 pt-1">or</h1>
+                        
+                        <div className="grid grid-cols-2 gap-4 pt-[20px]"> 
+                        <div className="flex grow text-justify justify-center text-[#3b60ba]">
+                                <LocalFile handleNewPreview={handleNewPreview}/> 
+                        </div>
+                        {/* "absolute right-[120px] mt-[100px] rounded-lg bg-white  m-6 h-[40px] w-[200px]  items-center shadow-lg"> */}
+                        <button  className="flex grow justify-center text-justify align-middle h-[30px] w-[115px] rounded-sm outline outline-[#5072c0] text-[#3b60ba] outline-offset-[3px]">
                             Google Drive
                         </button>
+                        </div>
+                    </div>
+
+                    {/* Submission Box 2 */}
+                    <div className="p-6 h-[250px] w-[550px] bg-[#c3cee3] rounded-xl shadow-lg items-center space-x-4 outline-dashed outline-[#768fcd] outline-offset-[-3px]">
+                
+                        <h3 className="flex grow text-left justify-start text-l font-bold pb-1 pt-1 px-3">Title*</h3>
+                        <input type="text" id="Title" className="bg-transparent border-b-2 border-blue-500 text-gray-900 pt-1.5 pb-1.5 text-sm block w-11/12 outline outline-0 transition-all after:absolute after:bottom-2 after:block after:w-11/12" placeholder="Title of your piece" required />
+ 
+                        <h3 className="flex grow text-left justify-start text-l font-bold pb-1 pt-7">Description</h3>
+                        <input type="text" id="Title" className="bg-transparent border-b-2 border-blue-500 text-gray-900 pt-1.5 pb-1.5 text-sm block w-11/12 outline outline-0 transition-all after:absolute after:bottom-2 after:block after:w-11/12" placeholder="Describe your piece" required />
+                        <p className="text-xs text-gray-400 pt-1"><em>Max 400 Characters</em></p>
                     </div>
                 </div>
-                <button type="submit" className="absolute left-[10px] mt-[100px] rounded-lg bg-white  m-6 h-[40px] w-[90px]  items-center shadow-lg">
+                <div>
+                    <button className="rounded-lg items-center pt-4 ml-20">
+                        <Link href="/submit"> + Add Additional Photos</Link>
+                    </button>
+                </div>
+                <button className="absolute right-[176px] mt-[100px] rounded-lg bg-white  m-6 h-[40px] w-[200px]  items-center shadow-lg">
                     <Link href="/">
-                        Cancel
+                        Save & Continue Later
                     </Link>
                 </button>
-                <button type="submit" onClick={handleSubmit} className="absolute right-[10px] mt-[100px] rounded-lg m-6 h-[40px] w-[90px] items-center text-white bg-[#ec4899] shadow-lg">
+                <button type="submit" onClick={handleSubmit} className="absolute right-[64px] mt-[100px] rounded-lg m-6 h-[40px] w-[90px] items-center text-white bg-[#ec4899] shadow-lg">
                     <Link href="/">
-                        Submit
+                        Review
                     </Link>
                 </button>
             </div>
