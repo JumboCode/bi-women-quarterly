00e41dbef267329b8cde8457092e484c703d4e8a
lydiachen01 <137934459+lydiachen01@users.noreply.github.com>
2024-03-13 10:07:57 -0400

    Completed the AdminGrid

--- src/components/AdminGrid/index.tsx
+++ src/components/AdminGrid/index.tsx
@@ -1,4 +1,11 @@
+/**
+ * An admin page to display a grid containnig all submissions and their related information
+ * @author Lydia Chen
+ * @author Lucien Bao 
+ */
+
 import Submission from "@/types/Submission";
+import { Box, Typography } from '@mui/material'
 import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
 import Button from '@mui/material/Button';
 
@@ -6,12 +13,25 @@ type Props = {
     submissionArray: Submission[];
 }
 
+const tagStyle = {
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+    color: "#415db3",
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    paddingBlock: "5px",
+    borderRadius: "10px",
+}
+
+const nameStyle = {
+    color: "#415db3",
+}
+
 const AdminGrid: React.FC<Props> = (properties) => {
     const props = properties.submissionArray;
 
     let rows = [];
     for (let i = 0; i < props.length; i++) {
-        const row = {
+        const submission = {
             id: i,
             issue: props[i].issue,
             type: props[i].medium,
@@ -23,7 +43,7 @@ const AdminGrid: React.FC<Props> = (properties) => {
             ratings: props[i].rating,
             notes: props[i].notes
         }
-        rows.push(row);
+        rows.push(submission);
     }
 
     const handleClick = (event: any, cellValues: any) => {
@@ -32,17 +52,129 @@ const AdminGrid: React.FC<Props> = (properties) => {
     }
 
     const columns: GridColDef[] = [
-        { field: "issue", headerName: "Issue", width: 100, type: "singleSelect", valueOptions: [1, 2, 3, 4, 5, 6, 7, 8, 9], editable: false },
-        { field: "type", headerName: "Type", width: 100, type: "singleSelect", valueOptions: ["Type 1", "Type 2", "Type 3"], editable: false },
-        { field: "title", headerName: "Title of Piece", width: 250, editable: false },
-        { field: "name", headerName: "BWQ Name", width: 200, editable: false },
-        { field: "status", headerName: "Status", width: 100, type: "singleSelect", valueOptions: ["Accepted", "Pending", "Rejected"], editable: false },
+        {
+            field: "issue",
+            headerName: "Issue",
+            width: 100,
+            type: "singleSelect",
+            valueOptions: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            editable: false,
+            cellClassName: "issue",
+            headerClassName: "issue-header",
+            renderCell: (cellValues) => {
+                return (
+                    <div style={tagStyle}>
+                        {cellValues.row.issue}
+                    </div>
+                );
+            }
+        },
+        {
+            field: "type",
+            headerName: "Type",
+            width: 150,
+            type: "singleSelect",
+            valueOptions: ["Type 1", "Type 2", "Type 3"],
+            editable: false,
+            cellClassName: "type",
+            headerClassName: "type-header",
+            renderCell: (cellValues) => {
+                return (
+                    <div style={tagStyle}>
+                        {cellValues.row.type}
+                    </div>
+                );
+            }
+        },
+        {
+            field: "title",
+            headerName: "Title of Piece",
+            width: 250,
+            editable: false,
+            cellClassName: "title",
+            headerClassName: "title-header",
+            renderCell: (cellValues) => {
+                return (
+                    <div style={nameStyle}>
+                        {cellValues.row.title}
+                    </div>
+                );
+            }
+        },
+        {
+            field: "name",
+            headerName: "BWQ Name",
+            width: 200,
+            editable: false,
+            cellClassName: "name",
+            headerClassName: "name-header",
+            renderCell: (cellValues) => {
+                return (
+                    <div style={nameStyle}>
+                        {cellValues.row.name}
+                    </div>
+                );
+            }
+        },
+        { /* 
+           * We can also color the div with different colors depending on 
+           * the status (i.e. light green for Accepted, light red for Declined, etc.) 
+           * 
+           */
+            field: "status",
+            headerName: "Status",
+            width: 150,
+            type: "singleSelect",
+            valueOptions: ["Accepted", "Pending", "Rejected"],
+            editable: false,
+            cellClassName: "status",
+            headerClassName: "status-header",
+            renderCell: (cellValues) => {
+                return (
+                    <div style={tagStyle}>
+                        {cellValues.row.status}
+                    </div>
+                );
+            }
+        },
         // { field: "demographics", headerName: "Demographics", width: 200, type: "singleSelect", valueOptions: ["United States", "Canada", "Antarctica"], editable: false },
-        { field: "tags", headerName: "Tags", width: 200, editable: false },
-        { field: "ratings", headerName: "Rating", width: 100, type: "singleSelect", valueOptions: [1, 2, 3, 4, 5], editable: false },
-        { field: "notes", headerName: "Notes", width: 250, editable: false },
+        {
+            field: "tags",
+            headerName: "Tags",
+            width: 200,
+            editable: false,
+            cellClassName: "tags",
+            headerClassName: "tags-header",
+            renderCell: (cellValues) => {
+                return (
+                    <div style={tagStyle}>
+                        {cellValues.row.tags}
+                    </div>
+                );
+            }
+        },
+        {
+            field: "ratings",
+            headerName: "Rating",
+            width: 100,
+            type: "singleSelect",
+            valueOptions: [1, 2, 3, 4, 5],
+            editable: false,
+            cellClassName: "ratings",
+            headerClassName: "ratings-header",
+        },
+        {
+            field: "notes",
+            headerName: "Notes",
+            width: 250,
+            editable: false,
+            cellClassName: "notes",
+            headerClassName: "notes-header",
+        },
         {
             field: "Print",
+            cellClassName: "print",
+            headerClassName: "print-header",
             renderCell: (cellValues) => {
                 return (
                     <Button
@@ -59,14 +191,58 @@ const AdminGrid: React.FC<Props> = (properties) => {
     ];
 
     return (
-        <div>
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                slots={{ toolbar: GridToolbar }}
-                slotProps={{ toolbar: { showQuickFilter: true } }}
-                className="m-20 mt-0" />
-        </div>
+        /* move styling to index where the component is called */
+        <div className="bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-300">
+            <Typography
+                variant="h6"
+                fontWeight={700}
+                fontSize={24}
+                color="#415db3"
+                paddingTop={8}
+                paddingLeft={10}
+            >
+                Submissions
+            </Typography>
+
+            <Box
+                sx={{
+                    "& .issue, & .type, & .title, & .name, & .status, \
+                    & .tags, & .ratings, & .notes, & .print": {
+                        backgroundColor: "rgba(255, 255, 255, 0.3)",
+                        color: "#415db3",
+                        border: "none",
+                    },
+                    "& .issue-header, & .type-header, & .title-header, \
+                    & .name-header, & .status-header, & .tags-header, \
+                    & .ratings-header, & .notes-header, & .print-header": {
+                        color: "#415db3",
+                        // backgroundColor: 'rgba(255, 255, 255, 0.6)',
+                    },
+                }}
+            >
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    slots={{ toolbar: GridToolbar }}
+                    slotProps={{ toolbar: { showQuickFilter: true } }}
+                    className="mx-20 mt-0 border-none"
+                    getRowSpacing={params => ({
+                        top: params.isFirstVisible ? 0 : 3,
+                        bottom: params.isLastVisible ? 0 : 3
+                    })}
+                    sx={{
+                        '&, [class^=MuiDataGrid]': { border: 'none' },
+                        '& .MuiDataGrid-virtualScroller': {
+                            transform: 'rotateX(180deg)',
+                            paddingBlock: "20px"
+                        },
+                        '& .MuiDataGrid-virtualScrollerContent': {
+                            transform: 'rotateX(180deg)',
+                        },
+                    }}
+                />
+            </Box>
+        </div >
     );
 
 }
--- src/components/AdminHomePage/index.tsx
+++ src/components/AdminHomePage/index.tsx
@@ -1,6 +1,6 @@
 /**
- * Admin home page.
- * NOTE unfinished.
+ * Admin Homepage
+ * @author Lydia Chen
  * @author Lucien Bao
  */
 
@@ -30,7 +30,16 @@ export default function AdminHomePage() {
         { id: "author|title|date", issue: "6", medium: Mediums.Other, title: "Example F", author: "Author F", status: Statuses.Declined, /*demographics: "Canada",*/ tags: ["Other"], rating: 4, notes: "None", mainSubmission: blank, date: "3/3/2024" },
         { id: "author|title|date", issue: "7", medium: Mediums.Other, title: "Example G", author: "Author G", status: Statuses.Declined, /*demographics: "Antarctica",*/ tags: ["Other"], rating: 3, notes: "None", mainSubmission: blank, date: "3/3/2024" },
         { id: "author|title|date", issue: "8", medium: Mediums.Other, title: "Example H", author: "Author H", status: Statuses.Declined, /*demographics: "Antarctica",*/ tags: [], rating: 2, notes: "None", mainSubmission: blank, date: "1/1/1111" }, // tags is empty
-        { id: "author|title|date", issue: "9", medium: Mediums.Other, title: "Example I", author: "Author I", status: Statuses.Declined, /*demographics: "Antarctica",*/ rating: 1, notes: "None", mainSubmission: blank, date: "3/3/2024" } // has no tags
+        { id: "author|title|date", issue: "9", medium: Mediums.Other, title: "Example I", author: "Author I", status: Statuses.Declined, /*demographics: "Antarctica",*/ rating: 1, notes: "None", mainSubmission: blank, date: "3/3/2024" }, // has no tags
+        { id: "author|title|date", issue: "10", medium: Mediums.Fiction, title: "Example A", author: "Author A", status: Statuses.Approved, /*demographics: "United States",*/ tags: ["Family"], rating: 5, notes: "None", mainSubmission: blank, date: "3/3/2024" },
+        { id: "author|title|date", issue: "11", medium: Mediums.Fiction, title: "Example B", author: "Author B", status: Statuses.Approved, /*demographics: "United States",*/ tags: ["Love"], notes: "None", mainSubmission: blank, date: "3/3/2024" }, // has no rating
+        { id: "author|title|date", issue: "12", medium: Mediums.Nonfiction, title: "Example C", author: "Author C", status: Statuses.Pending, /*demographics: "United States",*/ tags: ["Other"], rating: 5, notes: "One!", mainSubmission: blank, date: "3/3/2024" },
+        { id: "author|title|date", issue: "13", medium: Mediums.Poetry, title: "Example D", author: "Author D", status: Statuses.Pending, /*demographics: "Canada",*/ tags: ["Tag1", "Tag2"], rating: 5, mainSubmission: blank, date: "3/7/2024" }, // has no notes
+        { id: "author|title|date", issue: "14", medium: Mediums.VisualArt, title: "Example E", author: "Author E", status: Statuses.Pending, /*demographics: "Canada",*/ tags: ["Other"], rating: 5, notes: "None", mainSubmission: blank, date: "3/3/2024" },
+        { id: "author|title|date", issue: "15", medium: Mediums.Other, title: "Example F", author: "Author F", status: Statuses.Declined, /*demographics: "Canada",*/ tags: ["Other"], rating: 4, notes: "None", mainSubmission: blank, date: "3/3/2024" },
+        { id: "author|title|date", issue: "16", medium: Mediums.Other, title: "Example G", author: "Author G", status: Statuses.Declined, /*demographics: "Antarctica",*/ tags: ["Other"], rating: 3, notes: "None", mainSubmission: blank, date: "3/3/2024" },
+        { id: "author|title|date", issue: "17", medium: Mediums.Other, title: "Example H", author: "Author H", status: Statuses.Declined, /*demographics: "Antarctica",*/ tags: [], rating: 2, notes: "None", mainSubmission: blank, date: "1/1/1111" }, // tags is empty
+        { id: "author|title|date", issue: "18", medium: Mediums.Other, title: "Example I", author: "Author I", status: Statuses.Declined, /*demographics: "Antarctica",*/ rating: 1, notes: "None", mainSubmission: blank, date: "3/3/2024" } // has no tags
     ];
 
     return (
