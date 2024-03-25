7ddf175a8b5d02a25a51f9de9bd9f982463a9fc4
pgyy <peigen.yuan@tufts.edu>
2024-03-24 23:41:01 -0400

    changes made during meeting

--- src/components/HomePage/SubmissionThumbnail.tsx
+++ src/components/HomePage/SubmissionThumbnail.tsx
@@ -35,24 +35,34 @@ const SubmissionThumbnail: React.FC<Props> = props => {
     // tags?: string[] | undefined;
     // rating?: number | undefined;
     // notes?: string | undefined;
+    let submission_date: string = submission.date;
+    // console.log(submission_date);
+
+    function formatDate(submission.date: string): string {
+        const date = new Date(submission.date);
+        const options = { month: 'short', day: '2-digit', year: 'numeric' };
+        return date.toLocaleDateString('en-US', options);
+    }
+
 
     /*----------------------------------------*/
     /* --------------- Main UI -------------- */
     /*----------------------------------------*/
     return (
         <div>
-            <div onClick={() => {console.log('Button clicked!')}} className="flex-col items-start bg-[#F0C2FE] hover:bg-[#385FB8] cursor-pointer m-2 p-2.5 transition-colors rounded-lg">
+            <div onClick={() => {console.log('Button clicked!')}} 
+            className="group block flex-col items-start bg-[#F0C2FE] hover:bg-[#385FB8] cursor-pointer m-2 p-2.5 transition-colors rounded-lg hover:text-[#ffffff]">
                 <div className="max-w-md min-w-min w-2/4 bg-gray-200 m-auto">
                     <img src={submission.mainSubmission.imageUrl} className="max-w-full rounded-lg"></img>
                 </div>
                 <div className="max-w-md min-w-min w-2/4">
-                    <div className="text-[#385FB8] font-bold hover:text[#ECEFF4]">{submission.title}</div>
+                    <div className="text-[#385FB8] font-bold md:text-xl lg:text-2xl xl:text-2xl group-hover:text-white">{submission.title}</div>
                     {/* <div className="font-normal">{contentDriveUrl}</div> */}
                 </div>
-                <div className="max-w-md min-w-min w-2/4">
-                    <div className="text-[#385FB8] font-bold hover:text[#ECEFF4]">{submission.date}</div>
+                <div className="max-w-md min-w-min w-2/4 group-hover:text-white">
+                    <div className="text-[#385FB8] md:text-lg group-hover:text-white">{submission.date}</div>
                     {/* <div className="font-normal">{contentDriveUrl}</div> */}
-                </div>
+                </div> 
             </div>
             <br />
         </div>
