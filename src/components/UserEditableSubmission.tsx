e88294a5f18bcc3227b5f441598260d0a4c4759e
ALLISON ZHANG <andihedralwings@gmail.com>
2024-04-07 15:25:40 -0400

    fixed more of the styling

--- src/components/UserEditableSubmission.tsx
+++ src/components/UserEditableSubmission.tsx
@@ -1,5 +1,5 @@
 // ... other imports
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent } from 'react'; 
 import Submission from '@/types/Submission';
 import PreviewType from '@/types/PreviewType';
 import Issues from '@/types/Issues';
@@ -108,140 +108,160 @@ const UserEditableSubmission: React.FC<Props> = ({ submission: initialSubmission
   /* --------------- Main UI -------------- */
   /*----------------------------------------*/
   const body = editOn ? (
-    <div className="submission-container flex flex-col h-screen" style={{backgroundImage: 'linear-gradient(to bottom right, #FFD3CB, #E7A5FF, #B3C9FF)'}}>
-      
-      <form onSubmit={handleEdit} className="flex flex-row justify-start mb-4">
-        <button type="submit" className="text-white px-4 py-2 mb-4 inline-block" style={{ color: '#395EB9', fontSize: '24px' }}>
-          &lt;- Back
-        </button>
-      </form>
-      <div className="title-date flex items-center justify-between mb-4">
-      <div className="edit-submission-title text-3xl ml-10 mb-4" style={{ fontFamily: 'Open Sans', fontSize: '32.73px', fontWeight: 700, lineHeight: '44.57px', textAlign: 'left', color: '#395EB9'}}>Edit Submission</div>
-        <div className="flex items-center">
-          <form>
-            <div className="issue-type text font-bold">
-              <label>Issue: </label>
-              <select name="issue" style={{ background: '#FADADD', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', padding: '6px', borderRadius: '4px'}}>
-                  <option value="Current">{Issues.Current}</option>
-                  <option value="Next">{Issues.Next}</option>
-                  <option value="None">{Issues.None}</option>
-              </select>
-              &nbsp;&nbsp;&nbsp;&nbsp;
-              <label>Type: </label>
-              <select name="issue" style={{ background: '#FADADD', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', padding: '6px', borderRadius: '4px' }}>
-                  <option value="Submission">{PreviewType.Submission}</option>
-                  <option value="addRef">{PreviewType.AdditionalReference}</option>
-              </select>
+      <div className="flex flex-row h-screen UserEdit-container m-[0px] justify-around overflow-y-scroll" style={{backgroundImage: 'linear-gradient(to bottom right, #FFD3CB, #E7A5FF, #B3C9FF)'}}>
+        <div className="w-[70%] mt-[5%]">
+          <form onSubmit={handleEdit} className="flex flex-row justify-start mb-4">
+            <button type="submit" className="text-white px-4 py-2 mb-4 inline-block fixed top-0 left-0" style={{ color: '#395EB9', fontSize: '24px' }}>
+              &lt;- Back
+            </button>
+          </form>
+          <div className="title-date flex items-center justify-between mb-4">
+          <div className="edit-submission-title text-3xl mb-4" style={{fontSize: '32.73px', fontWeight: 700, lineHeight: '44.57px', textAlign: 'left', color: '#395EB9'}}>Edit Submission</div>
+            <div className="flex items-center">
+              <form>
+                <div className="issue-type text font-bold">
+                  <label>Issue: </label>
+                  <select name="issue" style={{ background: '#FADADD', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', padding: '6px', borderRadius: '4px'}}>
+                      <option value="Current">{Issues.Current}</option>
+                      <option value="Next">{Issues.Next}</option>
+                      <option value="None">{Issues.None}</option>
+                  </select>
+                  &nbsp;&nbsp;&nbsp;&nbsp;
+                  <label>Type: </label>
+                  <select name="issue" style={{ background: '#FADADD', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', padding: '6px', borderRadius: '4px' }}>
+                      <option value="Submission">{PreviewType.Submission}</option>
+                      <option value="addRef">{PreviewType.AdditionalReference}</option>
+                  </select>
+                </div>
+              </form>
             </div>
+          {/* <div className="submission-date text-white pr-10">
+            Submitted: {submission.date}
+          </div> */}
+        </div>
+        
+        <div className="flex flex-row  w-[100%] justify-between">
+          <img
+            src={submission.mainSubmission.imageUrl}
+            alt="Submission"
+            className="max-w-[40%] mr-4 rounded-lg"
+          />
+          
+          <div className="flex-col items-center py-2 UserEdit-textbox max-w-[55%] w-[100%]">
+            <div className="title text-xl border-b border-blue-500 p-[5%] text-left"> 
+              <b style={{ color: "#395EB9" }}>Title*</b>
+              <br></br>
+              <span style={{ fontSize: "14px" }}  contentEditable="true">{submission.title}</span>
+            </div>
+            <div className="image-description text-xl text-black p-[5%] text-left">
+              <b style={{ color: "#395EB9" }}>Description</b>
+              <br></br>
+              <span style={{ fontSize: "14px" }}  contentEditable="true">{submission.mainSubmission.description}</span>
+            </div>
+          </div>
+        </div>
+        
+        <div className="artist-statement mt-4 pl-10">
+          <div className="font-bold">Artist Statement</div>
+          <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Quasi sint pariatur, praesentium, accusantium hic ut enim repellendus 
+            ratione ipsum, illo voluptatem. Vel pariatur adipisci quidem dolorum, 
+            exercitationem dicta. Vero, officia? Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Tenetur nobis temporibus iusto odio vitae amet ex, 
+            nemo quae veniam rem dolore sequi aliquam eius even.
+          </div>
+        </div>
+        
+        <div className="bottom-0 right-0 w-full flex flex-row justify-end">
+          <form onSubmit={handleSave} className="bg-pink p-3 flex">
+            <button type="button" onClick={handleEdit} className="mr-2 UserEdit-bottombutton">
+              Save & Continue Later
+            </button>
+            <button type="submit" className="UserEdit-bottombuttonred">
+              Submit
+            </button>
           </form>
         </div>
-        {/* <div className="submission-date text-white pr-10">
-          Submitted: {submission.date}
-        </div> */}
-      </div>
-      
-      <div className="flex flex-row pl-10">
-        <img
-          src={submission.mainSubmission.imageUrl}
-          alt="Submission"
-          className="w-60 h-400 mr-4 rounded-lg"
-        />
-        <div className="flex-col items-center py-2 bg-[#eac4f9] rounded-lg drop-shadow-[0_0px_10px_rgba(0,0,0,0.25)] max-w-[40%] w-[100%]">
-          <div className="title text-xl border-b border-blue-500 p-[5%] text-left">
-            <b style={{ color: "#395EB9" }}>Title*</b>
-            <br></br>
-            <span style={{ fontSize: "14px" }}  contentEditable="true">{submission.title}</span>
-          </div>
-          <div className="image-description text-xl text-black p-[5%] text-left">
-            <b style={{ color: "#395EB9" }}>Description</b>
-            <br></br>
-            <span style={{ fontSize: "14px" }}  contentEditable="true">{submission.mainSubmission.description}</span>
-          </div>
-        </div>
-      </div>
-      
-      <div className="artist-statement mt-4 pl-10">
-        <div className="font-bold">Artist Statement</div>
-        <div>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-          Quasi sint pariatur, praesentium, accusantium hic ut enim repellendus 
-          ratione ipsum, illo voluptatem. Vel pariatur adipisci quidem dolorum, 
-          exercitationem dicta. Vero, officia? Lorem, ipsum dolor sit amet consectetur 
-          adipisicing elit. Tenetur nobis temporibus iusto odio vitae amet ex, 
-          nemo quae veniam rem dolore sequi aliquam eius even.
-        </div>
-      </div>
-      
-      <div className="absolute bottom-0 left-0 w-full flex justify-between">
-        <button type="button" className="mr-2 px-10 rounded shadow "style={{ color: '#395EB9',  boxShadow: '0px 0px 5px 0px #00000040'}}>
-          Delete Submission
-        </button>
-        <form onSubmit={handleSave} className="bg-pink p-3 flex items-center ">
-          <button type="button" onClick={handleEdit} className="mr-2 rounded shadow" style={{ color: '#395EB9'}}>
-            Save & Continue Later
-          </button>
-          <button type="submit" className="rounded shadow text-white" style={{ background: '#FF4395',  boxShadow: '0px 0px 5px 0px #00000040'}}>
-            Submit
-          </button>
-        </form>
-      </div>
+      </div>  
     </div>
   ) : (
-    <div className="submission-container flex flex-col h-screen" style={{backgroundImage: 'linear-gradient(to bottom right, #FFD3CB, #E7A5FF, #B3C9FF)'}}>
-      <form onSubmit={handleEdit} className="flex flex-row justify-start mb-4">
-      <button type="submit" className="text-white px-4 py-2 mb-4 inline-block" style={{ color: '#395EB9', fontSize: '24px' }}>
-          &lt;- Back
-        </button>
-      </form>
-      <div className="title-date flex items-center justify-between mb-4">
-        <div className="title text-xl font-bold">
-          {submission.title}
+    <div className="flex flex-row h-screen UserEdit-container m-[0px] justify-around overflow-y-scroll" style={{backgroundImage: 'linear-gradient(to bottom right, #FFD3CB, #E7A5FF, #B3C9FF)'}}>
+      <div className="mt-[5%] w-[70%] ">
+        
+        <div className="title-date flex items-center justify-between mb-4 ">
+          <div className="flex">
+            <div className="edit-submission-title text-3xl mb-4" style={{fontSize: '32.73px', fontWeight: 700, lineHeight: '44.57px', textAlign: 'left', color: '#395EB9'}}>
+              View Submission
+            </div>
+            <button className="ml-[50px]">
+                Google Drive
+            </button>
+          </div>
+            
+          <div className="flex items-center">
+            <form>
+              <div className="issue-type text font-bold">
+                <label>Issue: </label>
+                {submission.issue}
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <label>Type: </label>
+                {submission.mainSubmission.type}
+              </div>
+            </form>
+          </div>
         </div>
-        <div className="submission-date text-white pr-10">
-          Submitted: {submission.date}
+        <div className="flex flex-row  w-[100%] justify-between">
+          <img 
+            src={submission.mainSubmission.imageUrl}
+            alt="Submission"
+            className="max-w-[40%] mr-4 rounded-lg"
+          />
+          
+          <div className="flex-col items-center py-2 UserEdit-textbox max-w-[55%] w-[100%]">
+            <div className="title text-xl p-[5%] text-left"> 
+              <b style={{ color: "#395EB9" }}>Title*</b>
+              <br></br>
+              <span style={{ fontSize: "14px" }}  contentEditable="true">{submission.title}</span>
+            </div>
+            <div className="image-description text-xl text-black p-[5%] text-left">
+              <b style={{ color: "#395EB9" }}>Description</b>
+              <br></br>
+              <span style={{ fontSize: "14px" }}  contentEditable="true">{submission.mainSubmission.description}</span>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="flex items-center justify-between mb-4">
-        <div className="issue-type">
-          <span className="font-bold">Issue:</span> {submission.issue}&nbsp;&nbsp;&nbsp;&nbsp;
-          <span className="font-bold">Type:</span> {submission.mainSubmission.type}
+        
+        <div className="mt-[10%] ">
+          <div className="font-bold">Artist Statement</div>
+          <div className="flex-col items-center py-2 p-[50px] mb-[10%] UserEdit-textbox max-w-[100%] ">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Quasi sint pariatur, praesentium, accusantium hic ut enim repellendus 
+            ratione ipsum, illo voluptatem. Vel pariatur adipisci quidem dolorum, 
+            exercitationem dicta. Vero, officia? Lorem, ipsum dolor sit amet consectetur 
+            adipisicing elit. Tenetur nobis temporibus iusto odio vitae amet ex, 
+            nemo quae veniam rem dolore sequi aliquam eius even.
+          </div>
         </div>
-      </div>
-      <div className="flex flex-row">
-        <img
-          src={submission.mainSubmission.imageUrl}
-          alt="Submission"
-          className="w-60 h-400 mr-4"
-        />
-        <div className="image-description text-black">
-          Description: {submission.mainSubmission.description}
-        </div>
-      </div>
-      <div className="artist-statement mt-4">
-        <div className="font-bold">Artist Statement</div>
-        <div>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-          Quasi sint pariatur, praesentium, accusantium hic ut enim repellendus 
-          ratione ipsum, illo voluptatem. Vel pariatur adipisci quidem dolorum, 
-          exercitationem dicta. Vero, officia? Lorem, ipsum dolor sit amet consectetur 
-          adipisicing elit. Tenetur nobis temporibus iusto odio vitae amet ex, 
-          nemo quae veniam rem dolore sequi aliquam eius eveniet optio non totam voluptas
-        </div>
-      </div>
-      <form onSubmit={handleSave} className="flex flex-row justify-between mt-10">
-        <button type="button" className="mr-auto">
-          Delete Submission
-        </button>
-        <div className="flex flex-row">
-          <button type="button" onClick={handleEdit} className="mr-2">
-            Edit
+        
+        <div className=" bottom-0 left-0 w-full flex justify-between my-[10%]">
+          <button type="button" className="UserEdit-bottombutton">
+            Delete Submission
           </button>
-          <button type="submit">
-            Save
-          </button>
+          <form onSubmit={handleSave} className="bg-pink p-3 flex items-center ">
+            <div className="submission-date text-white pr-10">
+              Submitted: {submission.date}
+            </div>
+            <button type="button" onClick={handleEdit} className="mr-2 rounded shadow UserEdit-bottombutton">
+              Edit
+            </button>
+            <button type="submit" className="UserEdit-bottombuttonred">
+              Done
+            </button>
+          </form>
         </div>
-      </form>
       </div>
+    </div>
   );
 
 
