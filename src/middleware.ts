47a4f92fef44f92c1b0d77c51e827dd689b09de8
Avery Hanna <avery.hanna@tufts.edu>
2024-04-07 00:16:16 -0400

    displays image in background after upload

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ["/api/webhook", "/submit", "/", "/api/submissions/add", "/api/submissions/get-by-user", "/api/issues/get", "/upload", "/$%7Bsubmission.mainSubmission.imageUrl%7D"],
+    publicRoutes: ["/api/webhook", "/submit", "/", "/api/submissions/add", "/api/submissions/get-by-user", "/api/issues/get", "/upload"],
     ignoredRoutes: ["/api/submissions/get"]
 });
 
