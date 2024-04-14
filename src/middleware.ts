5445fbfc660d8cc106e71232a53777749692348f
shreyasravi320 <shreyas.ravi@tufts.edu>
2024-04-14 17:47:50 -0400

    adjust middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes: ["/api/webhook", "/submit"],
+    publicRoutes: ["/api/webhook", "/submit", "/"],
     ignoredRoutes: [
         "/api/submissions/get",
         "/api/submissions/add",
