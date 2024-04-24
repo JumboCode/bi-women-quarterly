59b432611b2411519bfbace1f253493e4c821abf
austen_em <austen.money@gmail.com>
2024-04-23 22:34:34 -0400

    add delete endpoint

---
+++ src/pages/api/submissions/delete.ts
@@ -0,0 +1,26 @@
+/**
+ * Submission delete endpoint for mongoDB Submissions collection
+ * @author Austen Money
+ */
+
+// Import NextApiRequest and NextApiResponse
+import { NextApiRequest, NextApiResponse } from "next";
+
+// Import clientPromise
+import clientPromise from "@/lib/mongodb";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        const client = await clientPromise;
+
+        const db = client.db("BiWomenQuarterly");
+        const collect = db.collection("Submissions");
+
+        // Delete the document with the given ID
+        await collect.deleteOne({ "submission.id": req.query.id })
+
+        res.status(201).json({ success: true });
+    } catch (e) {
+        res.status(400).json({ success: false });
+    }
+};
