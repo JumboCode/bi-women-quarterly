31d82c773b70ce8650b34c98ffd8e041c6ce907e
Shreyas Ravi <shreyas.ravi@tufts.edu>
2024-04-29 19:47:26 +0000

    Fix undefined problems

--- src/pages/submit.tsx
+++ src/pages/submit.tsx
@@ -1,5 +1,12 @@
+import Submission from "@/types/Submission";
 import SubmissionForm from "@/components/SubmissionForm";
 
-export default function FileUploadForm() {
-    return <SubmissionForm />;
+type Props = {
+    finishSubmit: (body: FormData, submission: Submission) => void,
+    goBack: () => void,
+};
+
+export const FileUploadForm: React.FC<Props> = props => {
+    const { finishSubmit, goBack } = props;
+    return <SubmissionForm finishSubmit={finishSubmit} goBack={goBack}/>;
 }
